286f2ae1b8ac168c6d716880ade7ccfa819b68ab
Ben Allen <benos.9k@gmail.com>
2015-02-26 16:05:21 +1300

    sub in bresenham line algo from stackoverflow, fixes crash

--- src/RidgeConverter.hpp
+++ src/RidgeConverter.hpp
@@ -130,6 +130,81 @@ namespace skadi {
 			}
 		};
 
+		// http://stackoverflow.com/questions/10060046/drawing-lines-with-bresenhams-line-algorithm
+		static std::vector<index> bhm_line(int x1, int y1, int x2, int y2) {
+			std::vector<index> indices;
+			int x,y,dx,dy,dx1,dy1,px,py,xe,ye,i;
+			dx=x2-x1;
+			dy=y2-y1;
+			dx1=fabs(dx);
+			dy1=fabs(dy);
+			px=2*dy1-dx1;
+			py=2*dx1-dy1;
+			if(dy1<=dx1) {
+				if(dx>=0) {
+					x=x1;
+					y=y1;
+					xe=x2;
+				} else {
+					x=x2;
+					y=y2;
+					xe=x1;
+				}
+				//putpixel(x,y,c);
+				indices.emplace_back(x, y);
+				for(i=0;x<xe;i++) {
+					x=x+1;
+					if(px<0) {
+						px=px+2*dy1;
+					} else {
+						if((dx<0 && dy<0) || (dx>0 && dy>0)) {
+							y=y+1;
+						} else {
+							y=y-1;
+						}
+						px=px+2*(dy1-dx1);
+					}
+					//putpixel(x,y,c);
+					indices.emplace_back(x, y);
+				}
+			} else {
+				if(dy>=0) {
+					x=x1;
+					y=y1;
+					ye=y2;
+				} else {
+					x=x2;
+					y=y2;
+					ye=y1;
+				}
+				//putpixel(x,y,c);
+				indices.emplace_back(x, y);
+				for(i=0;y<ye;i++) {
+					y=y+1;
+					if(py<=0) {
+						py=py+2*dx1;
+					} else {
+						if((dx<0 && dy<0) || (dx>0 && dy>0)) {
+							x=x+1;
+						} else {
+							x=x-1;
+						}
+						py=py+2*(dx1-dy1);
+					}
+					//putpixel(x,y,c);
+					indices.emplace_back(x, y);
+				}
+			}
+
+			assert(!indices.empty());
+
+			if (indices[0].x != x1) {
+				// make sure the indices start at x1,y1
+				std::reverse(indices.begin(), indices.end());
+			}
+
+			return indices;
+		}
 
 		static std::vector<index> getLine(int x1, int x2, int y1, int y2) {
 			std::vector<index> indices;
@@ -300,9 +375,13 @@ namespace skadi {
 				float ns1 = e->getNode1()->sharpness;
 				float ns2 = e->getNode2()->sharpness;
 
-				std::vector<index> indices = getLine(
-					int(v1.x() * size), int(v2.x() * size),
-					int(v1.y() * size), int(v2.y() * size));
+				//std::vector<index> indices = getLine(
+				//	int(v1.x() * size), int(v2.x() * size),
+				//	int(v1.y() * size), int(v2.y() * size)
+				//);
+
+				// note: different arg order
+				std::vector<index> indices = bhm_line(v1.x() * size, v1.y() * size, v2.x() * size, v2.y() * size);
 
 				int i1 = 0;
 				int i2 = indices.size()-1;
